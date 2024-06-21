e65a5cd8cacb76c7de0e71b68188152e3c76ce39
Jakub Gąsiorek <kubagasiorek6@gmail.com>
2024-06-21 15:29:45 +0200

    Revert "Added Die component"
    This reverts commit eb2ce1a7c5e175bbe4231e33d889d70cf6d5de3b.

--- src/Die.jsx
+++
@@ -1,9 +0,0 @@
-/* eslint-disable react/prop-types */
-
-export default function Die(props) {
-    return (
-        <div className="die-face">
-            <h2 className="die-num">{props.value}</h2>
-        </div>
-    )
-}
