2ff913075073a45d312b3c8f1fc141738b1f2065
Jakub Gąsiorek <kubagasiorek6@gmail.com>
2024-06-21 15:33:33 +0200

    Added styles and genereating Dice element

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
-
+import React from "react"
+import Die from "./Die"
+import {nanoid} from "nanoid"
 
 export default function App() {
+  const [dice, setDice] = React.useState(allNewDice())
+    
+    function allNewDice() {
+        const newDice = []
+        for (let i = 0; i < 10; i++) {
+            newDice.push({
+                value: Math.ceil(Math.random() * 6), 
+                isHeld: false,
+                id: nanoid()
+            })
+        }
+        return newDice
+    }
+
+    const diceElements = dice.map(die => (
+      <Die key={die.id} value={die.value} />
+    ))
+
     return (
-      <div className="container">
-        <main></main>
-      </div>
+      <main>
+        <div className="dice-container">
+            {diceElements}
+        </div>
+      </main>
     )
 }
