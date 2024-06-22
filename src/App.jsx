a6a6ec36947df811dd1d2a6a34f2135f3e0aa414
Jakub Gąsiorek <kubagasiorek6@gmail.com>
2024-06-22 17:40:32 +0200

    Added a button that rolls new values and an ability to press each die

--- src/App.jsx
+++ src/App.jsx
@@ -17,15 +17,36 @@ export default function App() {
         return newDice
     }
 
-    const diceElements = dice.map(die => (
-      <Die key={die.id} value={die.value} />
-    ))
+    function rollDice() {
+      setDice(allNewDice())
+    }
 
-    return (
+    function holdDice(id) {
+      setDice(oldDice => oldDice.map(die => {
+          return die.id === id 
+          ? {...die, isHeld: !die.isHeld} 
+          :die
+      }))
+  }
+
+    const diceElements = dice.map(die => (
+      <Die 
+          key={die.id} 
+          value={die.value} 
+          isHeld={die.isHeld} 
+          holdDice={() => holdDice(die.id)}
+      />
+  ))
+
+  return (
       <main>
+        <h1 className="title">Tenzies</h1>
+        <p className="instructions">Roll until all dice are the same. 
+          Click each die to freeze it at its current value between rolls.</p>
         <div className="dice-container">
             {diceElements}
         </div>
+        <button className="roll-dice" onClick={rollDice}>Roll</button>
       </main>
-    )
+  )
 }
--- src/Die.jsx
+++ src/Die.jsx
@@ -2,7 +2,11 @@
 
 export default function Die(props) {
     return (
-        <div className="die-face">
+        <div 
+            className="die-face" 
+            style={{backgroundColor: props.isHeld ? "#59E391" : "#fff"}} 
+            onClick={props.holdDice}
+        >
             <h2 className="die-num">{props.value}</h2>
         </div>
     )
