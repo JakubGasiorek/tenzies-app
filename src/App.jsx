667cfd4ad75d89c4c9b2a379b5757053322a9214
Jakub Gąsiorek <kubagasiorek6@gmail.com>
2024-06-25 11:32:33 +0200

    Added confettii showing that the game was won

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import Die from "./Die"
 import {nanoid} from "nanoid"
+import Confetti from "react-confetti"
 
 export default function App() {
   const [dice, setDice] = React.useState(allNewDice())
@@ -63,6 +64,7 @@ export default function App() {
 
   return (
       <main>
+        {tenzies && <Confetti/>}
         <h1 className="title">Tenzies</h1>
         <p className="instructions">Roll until all dice are the same. 
           Click each die to freeze it at its current value between rolls.</p>
